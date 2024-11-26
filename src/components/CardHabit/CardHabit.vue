3769d0c6a23ae002adbf456ff1b13b55a593f2e0
Emilbou <emilien.boucard@edu.univ-fcomte.fr>
2024-11-26 11:25:55 +0100

    feat(push du test)

--- src/components/CardHabit/CardHabit.vue
+++ src/components/CardHabit/CardHabit.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import ProgressBarTitle from '../ProgressBarTitle/ProgressBarTitle.vue'
+const test = 'test'
 </script>
 <template>
   <div class="CardHabit">
+    <!-- test-->
     <ProgressBarTitle :progress="20" Text="props 1" />
     <ProgressBarTitle :progress="50" Text="props 2" />
     <ProgressBarTitle :progress="80" Text="Progress Bar Title" />
