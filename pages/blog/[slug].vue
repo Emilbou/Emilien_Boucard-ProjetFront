b2739731e60f2128973737f4d48990414c902092
Emilbou <emilien.boucard@edu.univ-fcomte.fr>
2024-12-03 13:37:03 +0100

    feat(amélioration de l'affichage du blog) : ajout d'une image Sanity et simplification de la structure du composant

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -16,24 +16,22 @@ const urlFor = (source: SanityImageSource) =>
 
 <template>
   <main
-    v-if="post"
-    class="container mx-auto min-h-screen max-w-3xl p-8 flex flex-col gap-4"
-  >
-    <a href="/" class="hover:underline">&larr; Back to posts</a>
+    v-if="post">
+    <a href="/blog/" >&larr; Back to posts</a>
     <img
       v-if="post.image"
-      :src="urlFor(post.image).width(550).height(310).url()"
+      :src="urlFor(post.image)?.width(550).height(310).url()"
       :alt="post?.title"
-      class="aspect-video rounded-xl"
       width="550"
       height="310"
     >
-    <h1 v-if="post.title" class="text-4xl font-bold mb-8">{{ post.title }}</h1>
-    <div class="prose">
+    <h1 v-if="post.title">{{ post.title }}</h1>
+    <div>
       <p v-if="post.publishedAt">
         Published: {{ new Date(post.publishedAt).toLocaleDateString() }}
       </p>
       <SanityContent v-if="post.body" :blocks="post.body" />
+      <SanityImage :asset-id="post.image.asset._ref" style="max-width: 100%;"/>
     </div>
   </main>
 </template>
