ad6147043c7a1f96f4929abae1629a00e133aaf8
Emilbou <emilien.boucard@edu.univ-fcomte.fr>
2024-12-04 16:24:49 +0100

    feat(composable urlFor) : mise en place d'un composable pour la gestion des urls d'image de sanity

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -1,19 +1,13 @@
 <script setup lang="ts">
 import type { SanityDocument } from "@sanity/client";
-import imageUrlBuilder from "@sanity/image-url";
-import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
 
+const {urlFor} = useSanityImage();
 
 const POST_QUERY = groq`*[_type == "post" && slug.current == $slug][0]`;
 const { params } = useRoute();
 
 const { data: post } = await useSanityQuery<SanityDocument>(POST_QUERY, params);
-const { projectId, dataset } = useSanity().client.config();
-const urlFor = (source: SanityImageSource) =>
-projectId && dataset
-? imageUrlBuilder({ projectId, dataset }).image(source)
-: null;
 
 if (!post.value) {
   //  navigateTo('/404')
