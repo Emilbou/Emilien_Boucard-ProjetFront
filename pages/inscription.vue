7c47228cf018490a70043dd961d1b15bcd6c6003
Emilbou <emilien.boucard@edu.univ-fcomte.fr>
2024-12-04 15:54:36 +0100

    feat(meta login/register)

--- pages/inscription.vue
+++ pages/inscription.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
 definePageMeta({layout: "none"})
+useSeoMeta({
+  title:"Page Register",
+  description: "Description de la Page Register",
+  ogTitle: "Page Register",
+  ogDescription: "Description de la Page Register",
+})
 </script>
 <template>
     <LoginForm :text-log="`S'inscrire`" :text-swap="'Déjà un compte ?'" :url-swap="'/connection'"/>
