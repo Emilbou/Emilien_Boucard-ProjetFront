1d4e659d6aba33bd09c28c1e8d14ed1de9c24f1e
Emilbou <emilien.boucard@edu.univ-fcomte.fr>
2024-12-09 10:45:59 +0100

    feat(refactorisation nouvelles données sanity)

--- components/SanityArray/SanityArray.vue
+++ components/SanityArray/SanityArray.vue
@@ -4,14 +4,17 @@ import type { SanityDocument } from "@sanity/client";
 const POST_QUERY = groq`*[_type == 'homepage']`;
 
 const { data: post } = await useSanityQuery<SanityDocument>(POST_QUERY);
+console.log(post);
 </script>
 
 <template>
     <div v-if="post" class="SanityArray">
-        <div  v-for="stat in post[0].hero.stats" :key="stat._key" class="SanityArray__container">
-            <div class="SanityArray__layout"><span>{{ stat.value }}</span> <span>{{ stat.text }}</span></div>
+        <div  v-for="stat in post[0].sectionAccueil.statistiques" :key="stat._key" class="SanityArray__container">
+            <div class="SanityArray__layout"><span>{{ stat.valeur }}</span> <span>{{ stat.texteExplicatif }}</span></div>
         </div>
-        
+        <!-- <pre>
+            {{ post }}
+        </pre> -->
     </div>
 </template>
 
@@ -29,7 +32,17 @@ const { data: post } = await useSanityQuery<SanityDocument>(POST_QUERY);
     }
     &__layout {
         display: flex;
-flex-direction: column
+flex-direction: column;
+span {
+    &:first-child {
+        font-size: 40px;
+        font-weight: bold;
+    }
+    &:last-child {
+        font-size: 20px;
+        font-weight: 300;
+}
+}
     }
 }
 </style>
