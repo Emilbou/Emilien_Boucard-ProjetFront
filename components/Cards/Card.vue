c8a3f639ecbe1d64052c4ebb994e9af3a3e8faf3
Emilbou <emilien.boucard@edu.univ-fcomte.fr>
2024-12-10 13:17:49 +0100

    feat(ajout de la gestion des cartes de tableau de bord) : intégration de la récupération des données et création d'un nouveau composant Card pour afficher les informations

---
+++ components/Cards/Card.vue
@@ -0,0 +1,89 @@
+<script setup lang="ts">
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+
+const date = ref()
+defineProps({
+  progress: {
+    type: Number,
+    default: 50,
+  },
+  todayUsers: {
+    type: Number,
+    default: 0,
+  },
+  monthlyUsers: {
+    type: Number,
+    default: 0,
+  },
+  totalAttempts: {
+    type: Number,
+    default: 0,
+  },
+  description: {
+    type: String,
+    default: 'description',
+  },
+  titre: {
+    type: String,
+    default: 'Titre',
+  },
+})
+</script>
+<template>
+  <div class="CardHabit">
+    <h2 class="CardHabit__Title">{{ titre }}</h2>
+    <h3>{{ description }}</h3>
+    <div>
+    <ProgressBarTitle :progress="progress" />
+  <ul>
+    <li>Participants aujourd'hui : {{ todayUsers }}</li>
+    <li>Participants ce mois-ci : {{ monthlyUsers }}</li>
+    <li>Participants totaux : {{ totalAttempts }}</li>
+  </ul>
+    <div class="CardHabit__dateSection">
+      <VueDatePicker v-model="date" :enable-time-picker="false"/>
+      <CheckBox />
+    </div>
+        <MyButton :disabled="false">Ajouter</MyButton>
+        <MyButton class="CardHabit__Button" :disabled="false">Voir l'historique</MyButton>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.CardHabit {
+  border: 0.25px solid $gray800;
+  border-radius: 5px;
+  padding: 1rem;
+  display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  .CardHabit__Title {
+    color: $primaryColor;
+    font-size: 2rem;
+    margin-bottom: 0;
+    margin-top: 0;
+    text-transform: uppercase;
+  }
+
+  .CardHabit__dateSection {
+    display: flex;
+    width: 100%;
+    gap: 1rem;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 1rem;
+  }
+  .CardHabit__Button {
+    background-color: $primaryDarkColor;
+    padding: rem(8px) rem(16px);
+  }
+}
+ul {
+  li::marker {
+    color: $primaryColor;
+    font-size: 1.5rem;
+  }
+}
+</style>
--- pages/app/dashboard.vue
+++ pages/app/dashboard.vue
@@ -1,12 +1,31 @@
 <script setup lang="ts">
-const response = ref(null);
+interface DashboardResponse {
+  globalHabits: Array<{
+    id: number;
+    progress: number;
+    user_id?: number;
+    title: string;
+    description: string;
+    is_global: number;
+    created_at: string;
+    today_users: number;
+    total_users: number;
+    total_completions: number;
+    total_attempts: number;
+    monthly_users: number;
+    completedToday: boolean;
+    success_rate: number;
+  }>;
+}
+
+const response = ref<DashboardResponse | null>(null);
 
 onMounted(async () => {
   const res = await fetch(`http://localhost:4000/dashboard`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
-      "Authorization": `Bearer ${useCookie("api_tracking_jwt").value}`,
+      Authorization: `Bearer ${useCookie("api_tracking_jwt").value}`,
     },
   });
   response.value = await res.json();
@@ -14,7 +33,9 @@ onMounted(async () => {
 </script>
 
 <template>
-    <div>
-        <pre>{{ response }}</pre>
-    </div>
+  <div>
+
+<CardsContainer v-if="response" :response="response"/>
+    <pre>{{ response }}</pre>
+  </div>
 </template>
