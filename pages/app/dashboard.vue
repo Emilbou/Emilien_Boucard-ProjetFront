20c7eae62fd490896a5f0e3ceebcaa0c9806be85
Emilbou <emilien.boucard@edu.univ-fcomte.fr>
2024-12-10 11:15:01 +0100

    feat(récupération data dashboard)

---
+++ pages/app/dashboard.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const response = ref(null);
+
+onMounted(async () => {
+  const res = await fetch(`http://localhost:4000/dashboard`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${useCookie("api_tracking_jwt").value}`,
+    },
+  });
+  response.value = await res.json();
+});
+</script>
+
+<template>
+    <div>
+        <pre>{{ response }}</pre>
+    </div>
+</template>
