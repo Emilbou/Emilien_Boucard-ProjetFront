1978e34d53d07ccbf88027fa73cb0d51d4b2c156
Emilbou <emilien.boucard@edu.univ-fcomte.fr>
2024-12-10 13:50:55 +0100

    feat(mise en place de useAsyncData)

--- pages/app/dashboard.vue
+++ pages/app/dashboard.vue
@@ -17,8 +17,11 @@ interface DashboardResponse {
     success_rate: number;
   }>;
 }
-const response = ref<DashboardResponse | null>(null);
-onMounted(async () => {
+
+const AddHabitTitre = ref("");
+const AddHabitDesc = ref("");
+
+const { data: response, refresh } = await useAsyncData<DashboardResponse>('dashboard', async () => {
   const res = await fetch(`http://localhost:4000/dashboard`, {
     method: "GET",
     headers: {
@@ -26,41 +29,45 @@ onMounted(async () => {
       Authorization: `Bearer ${useCookie("api_tracking_jwt").value}`,
     },
   });
-  response.value = await res.json();
+  return res.json();
 });
 
-const AddHabitTitre = ref("");
-const AddHabitDesc = ref("");
-
-function AddHabit(event: Event) {
+async function AddHabit(event: Event) {
   event.preventDefault();
-  const createHabit = fetch(`http://localhost:4000/habits`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "authorization": `Bearer ${useCookie("api_tracking_jwt").value}`
-    },
-    body: JSON.stringify({
-      title: AddHabitTitre.value,
-      description: AddHabitDesc.value,
-    }),
-  });
-  console.log(createHabit);
   
-  console.log("add habit");
+  try {
+    await fetch(`http://localhost:4000/habits`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "authorization": `Bearer ${useCookie("api_tracking_jwt").value}`
+      },
+      body: JSON.stringify({
+        title: AddHabitTitre.value,
+        description: AddHabitDesc.value,
+      }),
+    });
+    
+    AddHabitTitre.value = "";
+    AddHabitDesc.value = "";
+                                                                                        
+    await refresh();
+    
+  } catch (error) {
+    console.error('Erreur lors de l\'ajout de l\'habitude:', error);
+  }
 }
 </script>
 
 <template>
   <div>
-
-<CardsContainer v-if="response" :response="response"/>
+    <CardsContainer v-if="response" :response="response" />
     <pre>{{ response }}</pre>
 
-    <form style="display: flex; flex-direction: column;" @submit="AddHabit" >
-        <input v-model="AddHabitTitre" type="text">
-        <input v-model="AddHabitDesc" type="text">
-<button>submit</button>
+    <form style="display: flex; flex-direction: column;" @submit="AddHabit">
+      <input v-model="AddHabitTitre" type="text">
+      <input v-model="AddHabitDesc" type="text">
+      <button>submit</button>
     </form>
   </div>
 </template>
